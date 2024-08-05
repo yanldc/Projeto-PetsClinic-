824dbfa3f50226827287b4a356d7f8099475ae3d
Yan Lemke de Castro <112890992+yanldc@users.noreply.github.com>
2024-08-05 19:26:56 -0300

    Update Pet.js
    Criação do modelo de pets e seus atributos

---
+++ models/Pet.js
@@ -0,0 +1,26 @@
+const {DataTypes} = require('sequelize')
+const db = require('../db/connection')
+const Tutor = require('./Tutor')
+
+const Pet = db.define('Pet', {
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    species:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    carry:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    weight: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    },
+    dateOfBirth:{
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+})
