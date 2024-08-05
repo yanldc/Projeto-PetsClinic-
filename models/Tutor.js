8b9f2b382555658bffb65fd9b8c771839529dfc5
Yan Lemke de Castro <112890992+yanldc@users.noreply.github.com>
2024-08-05 19:14:55 -0300

    Update Tutor.js
    criação do modelo de tutor e seus atributos

---
+++ models/Tutor.js
@@ -0,0 +1,25 @@
+const {DataTypes} = require('sequelize')
+const db = require('../db/connection')
+
+const Tutor = db.define('Tutor', {
+    name:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    phone:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    dateOfBirth:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    zipCode:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+})
