1704a676bc018cbe8ebcbadcfbf50785b06bd453
Yan Lemke de Castro <112890992+yanldc@users.noreply.github.com>
2024-08-07 19:04:02 -0300

    fix
    fixed delete, now pets are delete with tutors

--- index.js
+++ index.js
@@ -1,139 +1,171 @@
-const express = require('express')
-const con = require('./db/connection')
+const express = require("express");
+const con = require("./db/connection");
 
-const Tutor = require('./models/Tutor')
-const Pet = require('./models/Pet')
+const Tutor = require("./models/Tutor");
+const Pet = require("./models/Pet");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-
-            /* TUTORES */
-
-//100% (eu acho)
-app.get('/tutors', async(req, res) => {  
-    const id = req.query.id
-    const phone = req.query.phone
-    const email = req.query.email
-    const date_of_birth = req.query.date_of_birth
-    const zip_code = req.query.zip_code
-
-    const tutors = await Tutor.findAll( {attributes: ['id','name','phone','email','date_of_birth','zip_code']})
-
-    const pets = await Pet.findAll( {attributes: ['id','name','species','carry','date_of_birth','weight','TutorId']})
-
-    const result = tutors.map(tutor => ({
-        tutor,
-        pets: pets.filter(pet => pet.TutorId === tutor.id)
-    }));
-
-    res.json(result);
-})
+/* TUTORES */
 
 //100% (eu acho)
-app.post('/tutors', async(req, res) => {
-    const name = req.body.name
-    const phone = req.body.phone
-    const email = req.body.email
-    const date_of_birth = req.body.date_of_birth
-    const zip_code = req.body.zip_code
+app.get("/tutors", async (req, res) => {
+  const id = req.query.id;
+  const phone = req.query.phone;
+  const email = req.query.email;
+  const date_of_birth = req.query.date_of_birth;
+  const zip_code = req.query.zip_code;
 
-    await Tutor.create({name, phone, email, date_of_birth, zip_code })
+  const tutors = await Tutor.findAll({
+    attributes: ["id", "name", "phone", "email", "date_of_birth", "zip_code"],
+  });
 
-    res.json(Tutor)
-})
+  const pets = await Pet.findAll({
+    attributes: [
+      "id",
+      "name",
+      "species",
+      "carry",
+      "date_of_birth",
+      "weight",
+      "TutorId",
+    ],
+  });
 
-//100% (eu acho)
-app.put('/tutor/:id', async (req, res) => { 
-    const id = req.params.id
-    const name = req.body.name
-    const phone = req.body.phone
-    const email = req.body.email
-    const date_of_birth = req.body.date_of_birth
-    const zip_code = req.body.zip_code
+  const result = tutors.map((tutor) => ({
+    tutor,
+    pets: pets.filter((pet) => pet.TutorId === tutor.id),
+  }));
 
-    const userData = {
-        id,
-        name,
-        phone,
-        email,
-        date_of_birth,
-        zip_code
-    }
-
-    await Tutor.update(userData, {where: {id: id}})
-    res.json(Tutor)
-})
+  res.json(result);
+});
 
 //100% (eu acho)
-app.delete('/tutor/:id', async (req, res) => {  
-    const id = req.params.id
+app.post("/tutors", async (req, res) => {
+  const name = req.body.name;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const date_of_birth = req.body.date_of_birth;
+  const zip_code = req.body.zip_code;
 
-    await Tutor.destroy({where: {id: id}})
-    res.json("status code 204")
-})
+  await Tutor.create({ name, phone, email, date_of_birth, zip_code });
 
+  res.json(Tutor);
+});
 
-                 /* PETS */
+//100% (eu acho)
+app.put("/tutor/:id", async (req, res) => {
+  const id = req.params.id;
+  const name = req.body.name;
+  const phone = req.body.phone;
+  const email = req.body.email;
+  const date_of_birth = req.body.date_of_birth;
+  const zip_code = req.body.zip_code;
 
-app.get('/pets', async (req, res) => {
-    const name = req.query.name
-    const species = req.query.species
-    const carry = req.query.carry
-    const weight = req.query.weight
-    const date_of_birth = req.query.date_of_birth
+  const userData = {
+    id,
+    name,
+    phone,
+    email,
+    date_of_birth,
+    zip_code,
+  };
 
-    const pet = await Pet.findAll( {attributes: ['id','name','species','carry','date_of_birth','weight','TutorId']})
+  await Tutor.update(userData, { where: { id: id } });
+  res.json(Tutor);
+});
 
-    res.json(pet)
-})
+//100% (eu acho)
+app.delete("/tutor/:id", async (req, res) => {
+  const id = req.params.id;
+
+    await Pet.destroy({ where: { TutorId: id } });
+    await Tutor.destroy({ where: { id: id } });
+
+});
+
+/* PETS */
+
+app.get("/pets", async (req, res) => {
+  const name = req.query.name;
+  const species = req.query.species;
+  const carry = req.query.carry;
+  const weight = req.query.weight;
+  const date_of_birth = req.query.date_of_birth;
+
+  const pet = await Pet.findAll({
+    attributes: [
+      "id",
+      "name",
+      "species",
+      "carry",
+      "date_of_birth",
+      "weight",
+      "TutorId",
+    ],
+  });
+
+  res.json(pet);
+});
 
 // funcional (se por um ID inexistente o trem quebra)
-app.post('/pet/:tutorId', async(req, res) => { 
-    const tutorId = req.params.tutorId
-    const name = req.body.name
-    const species = req.body.species
-    const carry = req.body.carry
-    const weight = req.body.weight
-    const date_of_birth = req.body.date_of_birth
+app.post("/pet/:tutorId", async (req, res) => {
+  const tutorId = req.params.tutorId;
+  const name = req.body.name;
+  const species = req.body.species;
+  const carry = req.body.carry;
+  const weight = req.body.weight;
+  const date_of_birth = req.body.date_of_birth;
 
-    await Pet.create({name, species, carry, weight, date_of_birth, TutorId: tutorId})
+  await Pet.create({
+    name,
+    species,
+    carry,
+    weight,
+    date_of_birth,
+    TutorId: tutorId,
+  });
 
-    res.json(Pet)
-})
-
-//100% (eu acho)
-app.put('/pet/:petId/tutor/:tutorId', async (req, res) => { 
-    const tutorId = req.params.tutorId
-    const petId = req.params.petId
-    const name = req.body.name
-    const species = req.body.species
-    const carry = req.body.carry
-    const weight = req.body.weight
-    const date_of_birth = req.body.date_of_birth
-
-    const userData = {
-        name,
-        species,
-        carry,
-        weight,
-        date_of_birth
-    }
-
-    await Pet.update(userData, {where: {id: petId, TutorId: tutorId }})
-    res.json(Pet)
-})
+  res.json(Pet);
+});
 
 //100% (eu acho)
-app.delete('/pet/:petId/tutor/:tutorId', async (req, res) => {  
-    const tutorId = req.params.tutorId
-    const petId = req.params.petId
+app.put("/pet/:petId/tutor/:tutorId", async (req, res) => {
+  const tutorId = req.params.tutorId;
+  const petId = req.params.petId;
+  const name = req.body.name;
+  const species = req.body.species;
+  const carry = req.body.carry;
+  const weight = req.body.weight;
+  const date_of_birth = req.body.date_of_birth;
 
-    await Pet.destroy({where: {id: petId, TutorId: tutorId }})
-    res.json("status code 204")
-})
+  const userData = {
+    name,
+    species,
+    carry,
+    weight,
+    date_of_birth,
+  };
 
-con.sync()
-   .then(() => {app.listen(3000)} )
-   .catch((err) => console.log(err))
+  await Pet.update(userData, { where: { id: petId, TutorId: tutorId } });
+  res.json(Pet);
+});
+
+//100% (eu acho)
+app.delete("/pet/:petId/tutor/:tutorId", async (req, res) => {
+  const tutorId = req.params.tutorId;
+  const petId = req.params.petId;
+
+  await Pet.destroy({ where: { id: petId, TutorId: tutorId } });
+
+  res.json("status code 204");
+});
+
+con
+  .sync()
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
